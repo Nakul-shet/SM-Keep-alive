36c3e3e8888642a9fe14541629e56d4079b2adf5
Nakul N Shet <nakulnshet1@gmail.com>
2024-09-28 10:36:55 +0530

    added env

--- keep-alive.js
+++ keep-alive.js
@@ -1,22 +1,36 @@
 const https = require('https');
 
-const RENDER_URL = 'https://your-app-name.onrender.com'; // Replace with your Render URL
+const AUTH_URL = 'https://schedule-management-authentication.onrender.com';
+const BACKEND_URL = "https://schedule-management-api.onrender.com";
 const INTERVAL = 14 * 60 * 1000; // 14 minutes in milliseconds
 
-function keepAlive() {
-  https.get(RENDER_URL, (res) => {
+function keepAliveAuth() {
+  https.get(AUTH_URL, (res) => {
     if (res.statusCode === 200) {
-      console.log('Server pinged successfully');
+      console.log('Auth pinged successfully');
     } else {
-      console.log('Server ping failed');
+      console.log('Auth ping failed');
     }
   }).on('error', (err) => {
     console.error('Error pinging server:', err);
   });
 }
 
+function keepAliveBackend() {
+    https.get(BACKEND_URL, (res) => {
+      if (res.statusCode === 200) {
+        console.log('Backend pinged successfully');
+      } else {
+        console.log('Backend ping failed');
+      }
+    }).on('error', (err) => {
+      console.error('Error pinging server:', err);
+    });
+  }
+
 // Run the keep-alive function immediately and then every INTERVAL
-keepAlive();
+keepAliveBackend();
+keepAliveAuth();
 setInterval(keepAlive, INTERVAL);
 
 console.log('Keep-alive script is running...');
